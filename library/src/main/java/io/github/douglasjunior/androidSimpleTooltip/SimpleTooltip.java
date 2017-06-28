def5da07d56229f7211950575f0d92b09a1204b3
Jaap Manenschijn <jaap@myorder.nl>
2017-06-28 16:30:17 +0200

    fade style

--- library/src/main/java/io/github/douglasjunior/androidSimpleTooltip/SimpleTooltip.java
+++ library/src/main/java/io/github/douglasjunior/androidSimpleTooltip/SimpleTooltip.java
@@ -182,7 +182,7 @@ public class SimpleTooltip implements PopupWindow.OnDismissListener {
             @Override
             public void run() {
                 if (mRootView.isShown()) {
-                    mPopupWindow.setAnimationStyle(android.R.anim.fade_in);
+                    mPopupWindow.setAnimationStyle(R.style.AnimationPopup);
                     mPopupWindow.showAtLocation(mRootView, Gravity.NO_GRAVITY, mRootView.getWidth(), mRootView.getHeight());
                 } else {
                     Log.e(TAG, "Tooltip cannot be shown, root view is invalid or has been closed.");
